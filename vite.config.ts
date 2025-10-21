bcf84847cf9face0115dea5aea648027216eabfe
lorphy <lorphy@139.com>
2025-10-21 09:54:01 +0800

    modified vite deploy file,remove vercel related

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import vercel from 'vite-plugin-vercel';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server:{
     port:process.env.PORT as unknown as number,
-  }
-  plugins: [react(),vercel()],
+  },
+  plugins: [react()],
   base:process.env.VITE_BASE_PATH || '/questionaire',
   optimizeDeps: {
     exclude: ['lucide-react'],
